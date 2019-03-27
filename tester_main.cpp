a4ba7f8ebeeabd31215ca675be874c2f511283d5
ofermagen98 <ofermagen98@gmail.com>
2019-03-27 10:34:38 +0200

    Added vector library file, added operations

--- tester_main.cpp
+++ tester_main.cpp
@@ -1,8 +1,6 @@
-#define WITH_ACCESSES
 #include "matrix.h"
 #include <iostream>
 
-#ifdef WITH_ACCESSES
 int main(int argc, char const *argv[])
 {
 	/* code */
@@ -13,19 +11,7 @@ int main(int argc, char const *argv[])
 		{4,5}
 	});
 
- 	M.map([](const double& x){ return x - 1; });
+	M *= -2;
 	cout << M << endl;
 	return 0;
 }
-
-#else 
-int main(int argc, char const *argv[]){
-
-	const matrix<double> M({
-		{1,2,3},
-		{4,5,6}
-	});
-
-	return 0;
-}
-#endif
