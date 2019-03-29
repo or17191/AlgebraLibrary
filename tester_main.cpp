3520e210205d1c29c2b264c8f8fab24892fe2365
Ofer Magen <ofermagen@Ofers-MacBook-Pro.local>
2019-03-29 14:00:48 +0300

    added namespaces

--- tester_main.cpp
+++ tester_main.cpp
@@ -5,13 +5,12 @@
 int main(int argc, char const *argv[])
 {
 	/* code */
-
- 	matrix<double> M({
+ 	AlgebraTAU::matrix<double> M({
 		{0,2},
 	    {4,5}
 	});
 
- 	vector<double> v({1,2},false);
+ 	AlgebraTAU::vector<double> v({1,2},false);
  	v = -v;
  	v.map([](const double& x){return x*x;});
 	std::cout << v << std::endl;
